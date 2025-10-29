e92959d31231e87b8f3bd08be81b5a0a2285482d
Akash1-OP <akashsahana62@gmail.com>
2025-10-29 15:51:31 +0530

    some update

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,23 +1,24 @@
 import { User } from "../models/user.model.js";
-export const authCallback = async (req, res,next) => {
-  try {
-  const {id , firstName, lastName, imageUrl}= req.body;
 
-  // chack if user already exits
-  const user = await User.findOne({ clerkId: id });
+export const authCallback = async (req, res, next) => {
+	try {
+		const { id, firstName, lastName, imageUrl } = req.body;
 
-  if(user){
-    //signup
-    await User.create({
-      clerkId: id,
-      fullName: `${firstName ||""} ${lastName|| ""}`.trim(),
-      imageUrl,
-    });
-  }
-  res.status(200).json({ success: true });
-  } catch (error) {
-    console.log("Error in auth callback:", error);
-   next(error);
-    
-  }
-}
+		// check if user already exists
+		const user = await User.findOne({ clerkId: id });
+
+		if (!user) {
+			// signup
+			await User.create({
+				clerkId: id,
+				fullName: `${firstName || ""} ${lastName || ""}`.trim(),
+				imageUrl,
+			});
+		}
+
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.log("Error in auth callback", error);
+		next(error);
+	}
+};
